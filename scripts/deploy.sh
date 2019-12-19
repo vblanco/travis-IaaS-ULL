6b664b589f5e46d587b3ad0d582ccba494f8a738
Vicente Blanco <vblanco@ull.es>
2019-12-19 09:01:49 +0000

    Test open VPN

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Desplegando...."
+echo "Open VPN connection...."
+sudo vpnc my.conf
+echo "Connect to deployment backend...."
+ip a
+ip r
+echo "Disconnect VPN"
+sudo vpnc-disconnect
+
+#--
+
