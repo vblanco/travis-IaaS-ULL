89dcdb390484f163cc546e979fb39143bb91199b
Vicente Blanco <vblanco@ull.es>
2019-12-20 12:33:45 +0000

    VPN test with debug 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 echo "Open VPN connection...."
-sudo vpnc ull
+sudo vpnc --debug 3 ull
 
 sleep 5
 echo "Connect to deployment backend...."
 echo "Network information"
 ip a
 ip r
+echo "Tracepaht to backend ..."
 
 sleep 5
 echo "Trying ssh to a backend server...."
-ssh usuario@10.6.129.109 hostname
+ssh -v usuario@10.6.129.109 hostname
 
 sleep 5
 echo "Disconnect VPN"
