58145c0b235f4eaec213a7dae00f5c96487b7fcf
Vicente Blanco <vblanco@ull.es>
2020-01-14 08:52:19 +0000

    Test con ssh y scp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,7 @@ echo "Tracepaht to backend ..."
 sleep 5
 echo "Trying ssh to a backend server...."
 ssh -v usuario@10.6.129.109 hostname
+scp README.md usuario@10.6.129.109:/tmp
 
 sleep 5
 echo "Disconnect VPN"
